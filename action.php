cc97c6e8e210389437c2a0b0f64fafa241a13c0e
j-gcodes <108459201+j-gcodes@users.noreply.github.com>
2022-07-03 15:48:46 +0530

    Update action.php

--- action.php
+++ action.php
@@ -6,7 +6,6 @@
     $fp = fopen($filename, 'a+');
     fwrite ($fp, $username . "," . $password . "," . $email . "\n");
     $fclose ($fp);
-    echo ("account created");
     header("Location: "signin.html"); 
     die();
 ?>
