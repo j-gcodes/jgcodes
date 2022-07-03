3c07041616632da75bf35c43885c12b65334e7d4
j-gcodes <108459201+j-gcodes@users.noreply.github.com>
2022-07-03 15:56:26 +0530

    Update action.php

--- action.php
+++ action.php
@@ -6,6 +6,6 @@
     $fp = fopen($filename, 'a+');
     fwrite ($fp, $username . "," . $password . "," . $email . "\n");
     $fclose ($fp);
-    header("Location: "signin.html"); 
+    header("Location: "dasboard.html"); 
     die();
 ?>
